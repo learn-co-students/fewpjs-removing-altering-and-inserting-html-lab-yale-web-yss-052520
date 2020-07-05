3125b9f5b21fbcaeab0e47f4426fe83437a0f411
edensongjbs <edensongjbs@gmail.com>
2020-07-05 16:39:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+document.querySelector('main').remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+document.body.appendChild(newHeader)
+newHeader.innerHTML = 'YOUR-NAME is the champion'
