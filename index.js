a45e1fcfab0904c2d8f7cab55113d03c90827ebf
zayzay500 <lindsay.pierce@yale.edu>
2020-07-06 16:31:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const body = document.body
+document.querySelector('main#main').remove()
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Lindsay is the champion'
+body.appendChild(newHeader)
