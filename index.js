7a572e93a4e2f4437887dd1af1037f47600dfde0
Joao Cardoso <joao.cardoso@yale.edu>
2020-07-07 16:19:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let element = document.querySelector('main#main')
+element.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.innerHTML = "Joao is the champion!"
