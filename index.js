deaa2e19e4ddc14b924afc74c6bf229e271d3db2
mlkelly <maurak87@gmail.com>
2020-07-08 07:39:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+let main = document.body.querySelector("main#main");
+main.remove();
+
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Maura is the champion"
+
+document.body.appendChild(newHeader)
